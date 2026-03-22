913a35e51272622c7656c3a9844bee374453062a
rcorex <218065218+rcorex@users.noreply.github.com>
2026-03-22 15:09:02 +0000

    ACB Setup, ACB Reuse, Unload Flush, Audio Feed Sync, Buffer Full Play

--- xbmc/cores/VideoPlayer/MediaPipelineWebOS.h
+++ xbmc/cores/VideoPlayer/MediaPipelineWebOS.h
@@ -428,6 +428,7 @@ private:
 
   std::atomic<bool> m_videoClosed{true};
   std::atomic<bool> m_audioClosed{true};
+  std::atomic<int> m_speed{1000}; // DVD_PLAYSPEED_NORMAL
 
   std::mutex m_audioInfoMutex;
   std::string m_audioInfo;
