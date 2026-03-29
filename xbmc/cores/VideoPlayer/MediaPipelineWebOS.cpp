e02fdfe55b332c6dd4978be570428d90d2ed7f62
rcorex <218065218+rcorex@users.noreply.github.com>
2026-03-29 17:39:28 +0700

    Increase sleep duration in Resume method

--- xbmc/cores/VideoPlayer/MediaPipelineWebOS.cpp
+++ xbmc/cores/VideoPlayer/MediaPipelineWebOS.cpp
@@ -220,7 +220,7 @@ CMediaPipelineWebOS::~CMediaPipelineWebOS()
   if (auto activeAE = CServiceBroker::GetActiveAE())
   {
     activeAE->Resume();
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
 }
 
