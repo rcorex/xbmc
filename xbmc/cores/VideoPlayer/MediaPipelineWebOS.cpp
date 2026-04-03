dea9bbe5095d07126f9b7419dc3be1e9f4bdb6ef
rcorex <218065218+rcorex@users.noreply.github.com>
2026-04-03 21:30:25 +0700

    commented out 6db boost when downmixing

--- xbmc/cores/VideoPlayer/MediaPipelineWebOS.cpp
+++ xbmc/cores/VideoPlayer/MediaPipelineWebOS.cpp
@@ -1744,12 +1744,14 @@ void CMediaPipelineWebOS::ProcessAudio()
               m_audioLimiter.SetSamplerate(dstFormat.m_sampleRate);
               float volumeAmplification = m_processInfo.GetVideoSettings().m_VolumeAmplification;
               float boost = 0.0f;
+              /*
               if ((m_audioHint.codec == AV_CODEC_ID_AC3 || m_audioHint.codec == AV_CODEC_ID_EAC3) &&
                   m_audioCodec->GetFormat().m_channelLayout.Count() > 2 &&
                   dstFormat.m_channelLayout == CAEChannelInfo(AE_CH_LAYOUT_2_0))
               {
                 boost = 6.0f; //boost for AC3 & EAC3
               }
+              */
               m_volumeAmplificationBoost.store(boost);
               volumeAmplification += boost;
               m_audioLimiter.SetAmplification(std::pow(10.0f, volumeAmplification / 20.0f));
