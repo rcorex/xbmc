4db8988dd87fa7d7e87edc64c7079f0121817315
rcorex <218065218+rcorex@users.noreply.github.com>
2026-04-04 22:32:03 +0700

    revert earlier attempt to fix track change logic

--- xbmc/cores/VideoPlayer/MediaPipelineWebOS.cpp
+++ xbmc/cores/VideoPlayer/MediaPipelineWebOS.cpp
@@ -1202,7 +1202,7 @@ bool CMediaPipelineWebOS::FeedVideoData(const std::shared_ptr<CDVDMsg>& msg)
   {
     m_bitstream->Convert(data, static_cast<int>(size));
 
-    if (m_flushed && !m_bitstream->CanStartDecode() && !packet->recoveryPoint)
+    if (!m_bitstream->CanStartDecode())
     {
       CLog::LogF(LOGDEBUG, "Waiting for keyframe (bitstream)");
       return true;
