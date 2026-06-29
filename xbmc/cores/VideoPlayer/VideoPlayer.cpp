f6fc04c513b42c9f93c9830803d8e92ba84ea561
rcorex <218065218+rcorex@users.noreply.github.com>
2026-06-29 22:14:58 +0700

    Add check to ignore backward seeks at zero time
    Safely ignore backward seeks when timeline is at zero.

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -3598,6 +3598,9 @@ void CVideoPlayer::Seek(bool bPlus, bool bLargeStep, bool bChapterOverride)
 
   std::optional<int64_t> seekTarget;
   const int64_t time = GetTime();
+  if (!bPlus && time == 0)
+    return; // Safely ignore any backward seeks when the timeline is at zero
+
 #if defined(TARGET_WEBOS)
   bool accurate = true;
 #else
