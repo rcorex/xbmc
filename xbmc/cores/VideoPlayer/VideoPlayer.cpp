00559e2d6cafba58ffddd1ee75145e7723015648
rcorex <218065218+rcorex@users.noreply.github.com>
2026-07-04 00:06:23 +0700

    Revert Relative Seek non-EDL to not use SynchronizeDemuxer

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -3907,15 +3907,15 @@ bool CVideoPlayer::SeekTimeRelative(int64_t iTime)
   }
 
   CDVDMsgPlayerSeek::CMode mode;
-  mode.time = static_cast<double>(abstime);
+  mode.time = (int)iTime;
+  mode.relative = true;
   mode.backward = (iTime < 0) ? true : false;
   mode.accurate = false;
-  mode.restore = false;
   mode.trickplay = false;
   mode.sync = true;
 
   m_messenger.Put(std::make_shared<CDVDMsgPlayerSeek>(mode));
-  SynchronizeDemuxer();
+  m_processInfo->SetStateSeeking(true);
 
   m_callback.OnPlayBackSeek(abstime, iTime);
   m_processInfo->SeekFinished(iTime);
